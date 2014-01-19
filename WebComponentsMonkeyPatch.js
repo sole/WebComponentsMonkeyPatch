150741269e8cc655ad7041faa1cf825fbf5a4515
Soledad Penades <sole@mozilla.com>
2014-01-19 11:57:10 +0000

    Use only one function call

--- WebComponentsMonkeyPatch.js
+++ WebComponentsMonkeyPatch.js
@@ -1,20 +1,20 @@
 /**
  * Evidently superinspired by Chris Wilson's AudioContext-MonkeyPatch library,
- * and just patching webkitCreateShadowRoot to createShadowRoot
  */
 (function() {
 
-    if(  HTMLElement.prototype.webkitCreateShadowRoot !== undefined &&
-         HTMLElement.prototype.createShadowRoot === undefined ) {
+    // Unprefixed createShadowRoot
+    if( HTMLElement.prototype.webkitCreateShadowRoot !== undefined &&
+        HTMLElement.prototype.createShadowRoot === undefined ) {
 
         HTMLElement.prototype.createShadowRoot = HTMLElement.prototype.webkitCreateShadowRoot;
 
     }
 
+    // Alias document.register to document.registerElement
+    if('register' in document || 'registerElement' in document){
+        document.register = document.registerElement = document.register || document.register;
+    }
+
 })();
 
-(function() {
-  if('register' in document || 'registerElement' in document){
-    document.register = document.registerElement = document.register || document.register;
-  }
-})();
