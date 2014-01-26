9dfa56a41af5cf01ce4e6710b7e7b940f8067d6e
Soledad Penades <sole@mozilla.com>
2014-01-26 23:25:48 +0000

    Detecting if <template> is available.

--- example/playground.js
+++ example/playground.js
@@ -1,26 +1,41 @@
 window.onload = function() {
 
-  if( checkFeatures() ) {
-    init();
-  } else {
-    console.error('One or more of the features we want to test is not available in this browser.');
+  var missing = init();
+
+  if( missing.length !== 0 ) {
+    // Don't be content with a console.log. BE BOLD! Alert the hell out of it! 
+    window.alert('One or more of the features we want to test is not available in this browser: ' + missing.join(', ') );
   }
 
 
   // ---
 
-  function checkFeatures() {
-
-    // Are templates available?
-    // TODO how to do this?
-    
-    return true;
-  }
-
   function init() {
-    // Templates
-    document.getElementById('templateAdd').addEventListener('click', templateAdd);
-    templateAdd(); // Add an item so the list is not empty (and invisible)
+
+    var missingFeatures = [];
+
+    // TEMPLATES
+    // Are templates available? We'll figure it out by creating a <template>
+    // If it contains a .content attribute, it means the browser at least seems to know
+    // what it is doing!
+    var resTemplates = document.getElementById('templatesAvailable');
+    var btnTemplateAdd = document.getElementById('templateAdd');
+    var templateItem = document.createElement('template');
+
+    if(templateItem.content !== undefined) {
+      resTemplates.textContent = 'YAY';
+      btnTemplateAdd.addEventListener('click', templateAdd);
+      templateAdd(); // Add an item so the list is not empty (and invisible)
+    } else {
+      // nope
+      resTemplates.className = 'error';
+      resTemplates.textContent = 'NOPE';
+      btnTemplateAdd.disabled = true;
+      missingFeatures.push('templates');
+    }
+
+    return missingFeatures;
+
   }
 
   function templateAdd() {
