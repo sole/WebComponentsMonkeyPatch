f1ec890fb0a0adfc010c9aec6aca2fa80f1c54fe
Soledad Penades <sole@mozilla.com>
2014-02-06 00:18:57 +0000

    Creating custom component via constructor too

--- example/playground.js
+++ example/playground.js
@@ -35,23 +35,44 @@ window.onload = function() {
     }
 
 
+    function templateAdd() {
+      var t = document.querySelector("#fancyListItemTemplate");
+      var item = t.content.cloneNode(true);
+
+      // Populate content.
+      var parentList = document.querySelector('#templateInstancesParent');
+      item.querySelector('strong').innerHTML = 'Item number ' + parentList.children.length;
+      item.querySelector('em').textContent = "Anything is fine: " + Math.random();
+      parentList.appendChild(item);
+    }
+
+
     // CUSTOM ELEMENTS
-    // Can we register custom elements?
+    // Can we register custom elements by extending elements?
     // Wrapping in a try and throwing an exception too because currently registering an element
     // which doesn't start with x- won't work in Firefox.
     try {
       var btnExtendElement = document.getElementById('btnExtendElement');
+      var btnExtendElementCtor = document.getElementById('btnExtendElementCtor');
 
       if(document.registerElement !== undefined) {
 
         var buttonPrototype = Object.create(HTMLButtonElement.prototype);
 
-          document.registerElement('colourful-button', {
-            extends: 'button',
-            prototype: buttonPrototype
-          });
+        buttonPrototype.createdCallback = function() {
+          console.log('created');
+        };
 
-          btnExtendElement.addEventListener('click', makeColourfulButton, false);
+        var ColourfulButton = document.registerElement('colourful-button', {
+          extends: 'button',
+          prototype: buttonPrototype
+        });
+
+        // TODO: why do these methods differ in their output? First method doesn't 
+        // a) create objects with proper 'look' (like they are not inheriting_
+        // b) call the createdCallback
+        btnExtendElement.addEventListener('click', makeColourfulButton, false);
+        btnExtendElementCtor.addEventListener('click', makeColourfulButtonCtor, false);
 
       } else {
         throw new Error('document.registerElement not available');
@@ -63,28 +84,27 @@ window.onload = function() {
         btnExtendElement.disabled = true;
     }
 
+
+    function makeColourfulButton() {
+      var btn = document.createElement('colourful-button');
+      btn.innerHTML = 'hey';
+      document.getElementById('colourfulButtons').appendChild(btn);
+    }
+
+    function makeColourfulButtonCtor() {
+      var btn = new ColourfulButton();
+      btn.innerHTML = 'hey with constructor';
+      document.getElementById('colourfulButtons').appendChild(btn);
+    }
+
+
+
     return missingFeatures;
 
   }
 
 
-  function templateAdd() {
-    var t = document.querySelector("#fancyListItemTemplate");
-    var item = t.content.cloneNode(true);
   
-    // Populate content.
-    var parentList = document.querySelector('#templateInstancesParent');
-    item.querySelector('strong').innerHTML = 'Item number ' + parentList.children.length;
-    item.querySelector('em').textContent = "Anything is fine: " + Math.random();
-    parentList.appendChild(item);
-  }
-
-
-  function makeColourfulButton() {
-    var btn = document.createElement('colourful-button');
-    btn.innerHTML = 'hey';
-    document.getElementById('colourfulButtons').appendChild(btn);
-  }
 
 
 };
